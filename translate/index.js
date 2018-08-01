8f4423db4da09b24c8762c88604cb2d9fbd2df03
EustaceCheng <fbi0258zzz@gmail.com>
2018-08-01 20:38:52 +0800

    0801

---
+++ translate/index.js
@@ -0,0 +1,23 @@
+(function ( $ ) {
+ 
+    $.fn.work = function() {
+		
+		$("button").click(function(){
+			var i = $("#target")[0].value;
+			
+			
+			
+			
+			alert(i);
+			
+			
+			
+			
+			
+			
+		});
+		
+	
+    };
+ 
+}( jQuery ));
